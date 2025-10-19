50eb38f534c23075435980cfebfa61319460911c
LMX169 <wftqnxvgn4@privaterelay.appleid.com>
2025-10-19 22:17:02 +0800

    Add files via upload

---
+++ 101.c
@@ -0,0 +1,15 @@
+//202510304211
+//2124108137@qq.com
+//李景浩
+#include<stdio.h>
+int main()
+{
+    float degree=0;
+    scanf("%f",&degree);
+    float fahdegree=degree*5/9+32;
+    printf("%f",fahdegree);
+
+    return 0;
+
+
+}
